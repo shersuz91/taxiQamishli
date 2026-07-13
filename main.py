026d94c43c3b86933e3870caf404eeb15543ead3
SHERMAN-91 <shermansher402@gmail.com>
2026-07-12 23:25:20 -0600

    done

---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for, redirect, request, flash
+
+import requests
+
+TOKEN = "8880623007:AAHhPs56Ldqvelf1gxlXp1RFQzDQO3mgzUQ"
+url_ = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+url_Send = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+respons = requests.get(url_)
+print(respons.json())
+
+app = Flask(__name__)
+app.secret_key = "sher"
+
+@app.route("/")
+def home(message = ""):
+    return render_template("home.html")
+
+@app.route("/book", methods=["POST"])
+def book():
+    name = request.form["name"]
+    phone = request.form["phone"]
+    from_ = request.form["fromPlace"]
+    to_ = request.form["toPlace"]
+    day = request.form["day"]
+    note = request.form["note"]
+
+    dataList = [name, phone, from_, to_, day, note]
+
+    if not all(input for input in dataList[:-1]) :
+        return render_template("home.html",message =  dataList, len_ = len)
+
+    message = f"""🚕  حجز جديد
+        الاسم: {name}
+        الهاتف: {phone}
+        من: {from_}
+        الى: {to_}
+        الوقت: {day}
+        الملاحظات: {note}
+    """
+    print(name)
+    response = requests.post(url = url_Send, data={
+        "chat_id":8759699913,
+        "text":message
+    })
+    print(response.status_code)
+    print(response.json())
+    flash("تم حجز موعدك. سيصل فريقنا باسرع وقت ممكن.\nشكرا لصبركم!")
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug = True) 
---
+++ static/js/js.js
@@ -0,0 +1,26 @@
+var bookForm = document.getElementById("bookForm")
+
+bookForm.addEventListener("submit", function(event){
+    // event.preventDefault();
+
+    // var name = bookForm.querySelector("input[name = 'name']").value;
+    // var phone = bookForm.querySelector("input[name = 'phone']").value;
+    // var fromPlace = bookForm.querySelector("input[name = 'fromPlace']").value;
+    // var toPlace = bookForm.querySelector("input[name = 'toPlace']").value;
+    // var day = bookForm.querySelector("input[name = 'day']").value;
+    // var note = bookForm.querySelector("textarea").value;
+    // const message = `🚕  حجز جديد
+
+    // الاسم: ${name}
+    // الهاتف: ${phone}
+    // من: ${fromPlace}
+    // الى: ${toPlace}
+    // الوقت: ${day}
+    // الملاحظات: ${note}
+    // `
+
+    // const whatsappNumber = "14035889164";
+    // const url = `https://api.whatsapp.com/send?phone=${whatsappNumber}&text=${encodeURIComponent(message)}`;
+
+    // window.open(url, "_blank");
+})
